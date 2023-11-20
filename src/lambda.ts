a8f47bc56f07f0f51f0841ac15728e5e50fe712b
Imrul Khan <imrulkaiskhan@gmail.com>
2023-11-20 19:08:06 +0600

    enabled cors in nestjs

--- src/lambda.ts
+++ src/lambda.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app.module';
 let server: Handler;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.init();
